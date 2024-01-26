cf3aa180d415eeae883ed20586ca9b08ec0aecab
Sofie Van Dingenen <sofie.van.dingenen@iths.se>
2024-01-26 13:11:00 +0100

    added methods to main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
 
+public class Main {
+    public static ArrayList<BikeImpl> allBikes;
+    public static void main(String[] args) {
+        allBikes = new ArrayList<>();
+        BikeImpl cityBike = new Citybike();
+        BikeImpl mountainBike = new Moutainbike("tut tut tut", "Stockholm Central Station");
+        BikeImpl electricBike = new Electricbike("tooooooooooot");
+        BikeImpl foldningBike = new Foldnigbike();
+        BikeImpl singleSpeedBike = new SingleSpeedBike();
+
+        allBikes.add(cityBike);
+        allBikes.add(mountainBike);
+        allBikes.add(electricBike);
+        allBikes.add(foldningBike);
+        allBikes.add(singleSpeedBike);
+
+        ringBell();
+        turnOnLight();
+
+    }
+
+    public static void turnOnLight(){
+        for (BikeImpl bike : allBikes){
+            bike.lysa();
+        }
+    }
+
+    public static void ringBell(){
+        for(BikeImpl bike: allBikes){
+            bike.plinga();
+        }
     }
 }
